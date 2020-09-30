53a5ae63a4df58235225528deed38bd33caad29c
NazriGenfil <abienazri23@gmail.com>
2020-09-30 23:41:36 +0700

    menambahkan file

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from database.db import initialize_db
+from flask_restful import Api
+from flask_bcrypt import Bcrypt
+from resources.routes import initialize_routes #import jalur
+
+app = Flask(__name__)
+api = Api(app)
+Bcrypt = Bcrypt(app)
+
+app.config['MONGODB_SETTINGS'] = {
+    'host': 'mongodb+srv://POS:esjerukhangat@cluster0.gyggi.mongodb.net/test?retryWrites=true&w=majority'
+}
+
+initialize_db(app)
+initialize_routes(api) #memanggil jalur
+
+app.run(debug=True, port=5001)
